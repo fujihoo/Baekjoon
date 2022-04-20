df13735b8929a0f1ccded30e8dd402ed4d095d92
fujihoo <96091865+fujihoo@users.noreply.github.com>
2022-04-20 23:11:42 +0900

    [Bronze III] Title: 구구단, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+
+	scanf("%d", &a);
+
+	for (int i = 1; i <= 9; i++)
+	{
+		printf("%d * %d = %d\n", a, i, a * i);
+	}
+
+	return 0;
+}
