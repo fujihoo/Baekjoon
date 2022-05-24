eaf601ed99c823809a491b22d0d96a4b6bf88bfa
fujihoo <96091865+fujihoo@users.noreply.github.com>
2022-05-24 21:47:09 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "         ,r\'\"7\n"
+         << "r\`\-\_   ,\'  ,\/\n"
+         << " \\. \". L\_r\'\n"
+         << "   \`\~\\\/\n"
+         << "      \|\n"
+         << "      \|\n"
+         << endl;
+    return 0;
+}
