5dc8486f36f09e14608ca48ca13f9da99a99fcd5
fujihoo <96091865+fujihoo@users.noreply.github.com>
2022-05-27 00:21:01 +0900

    [Bronze III] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int N = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < i + 1; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
