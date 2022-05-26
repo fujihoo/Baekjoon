ac3ad0498b87e402aca829ce4830ac096aad5412
fujihoo <96091865+fujihoo@users.noreply.github.com>
2022-05-27 00:14:35 +0900

    [Bronze III] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int a = 0, b = 0, T = 0;
+
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i + 1 << ": " << a << " + " << b
+			 << " = " << a + b << "\n";
+	}
+	return 0;
+}
