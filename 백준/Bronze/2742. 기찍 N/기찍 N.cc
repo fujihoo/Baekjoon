84d331dd62a25cf4a269edcceb26ff1f02163210
fujihoo <96091865+fujihoo@users.noreply.github.com>
2022-05-26 23:54:49 +0900

    [Bronze III] Title: 기찍 N, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int N = 0;
+	
+	cin >> N;
+	for (int i = N; i > 0; i--)
+	{
+		cout << i  << "\n";
+	}
+
+	return 0;
+}
