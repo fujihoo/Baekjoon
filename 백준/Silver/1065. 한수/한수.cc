004902c73f9e44622c3155d4c01f5788c27f2d54
fujihoo <96091865+fujihoo@users.noreply.github.com>
2022-06-15 22:47:14 +0900

    [Silver IV] Title: 한수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n = 0, cnt = 0, hun = 0, ten = 0, one = 0;
+	
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (i < 100)
+		{
+			cnt++;
+		}
+		if (i >= 100)
+		{
+			hun = i / 100;
+			ten = (i % 100) / 10;
+			one = i % 10;
+			if (hun - ten == ten - one)
+			{
+				cnt++;
+			}
+		}
+	}
+	cout << cnt << "\n";
+
+	return 0;
+}
