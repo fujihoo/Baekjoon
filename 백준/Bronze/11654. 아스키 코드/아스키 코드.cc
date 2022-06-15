c9d1cf886df1b120056e497774494308b252bbd2
fujihoo <96091865+fujihoo@users.noreply.github.com>
2022-06-15 22:50:45 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	char input;
+	cin >> input;
+	cout << (int)input << "\n";
+	
+	return 0;
+}
