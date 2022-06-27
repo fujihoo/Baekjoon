a4617f66e26bcf918b2b29d415bfc2c07a614694
fujihoo <96091865+fujihoo@users.noreply.github.com>
2022-06-28 05:26:44 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << "\|\\\_\/\|" << "\n" << "\|q p\|   \/\}"
+		<< "\n" << "\( 0 \)\"\"\"\\" << "\n" << "\|\"\^\"\`    \|"
+		<< "\n" << "\|\|\_\/\=\\\\\_\_\|";
+
+	return 0;
+}
